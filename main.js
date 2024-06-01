9b06cf8c93cd7824ebea1be47aa3f09fce3de9b8
janitha-visna <visnajanitha@gmail.com>
2024-06-01 13:33:34 +0530

    render a  sphere in  html canvas

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const geometry = new THREE.SphereGeometry(3.64, 64);
+const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+//light
+const light = new THREE.PointLight(0Xffffff,100,100)
+light.position.set(0,10,10)
+scene.add(light)
+
+//camera
+
+const camera = new THREE.PerspectiveCamera(50,800/600)
+camera.position.z = 20
+scene.add(camera)
+
+//render
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(800,600)
+renderer.render(scene,camera)
