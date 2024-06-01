dd67ae447c3dd1eaa455b2ca64403cb3cc962f00
janitha-visna <visnajanitha@gmail.com>
2024-06-01 20:38:17 +0530

    update render according to screen size

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
-import "./style.css"
-
+import "./style.css";
 
 const scene = new THREE.Scene();
 
@@ -8,28 +7,54 @@ const geometry = new THREE.SphereGeometry(3.64, 64);
 const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
 
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh)
-
+scene.add(mesh);
 
 //sizes
 const sizes = {
-  width:window.innerWidth,
-  height:window.innerHeight,
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 //light
-const light = new THREE.PointLight(0Xffffff,100,100)
-light.position.set(0,10,10)
-scene.add(light)
+const light = new THREE.PointLight(0xffffff, 100, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
 
 //camera
 
-const camera = new THREE.PerspectiveCamera(45,sizes.width/sizes.height,0.1,100)
-camera.position.z = 20
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
 
 //render
-const canvas = document.querySelector(".webgl")
-const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(sizes.width,sizes.height)
-renderer.render(scene,camera)
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+
+//resize
+window.addEventListener("resize", () => {
+  //update sizes
+  console.log(window.innerWidth);
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+
+  // Render the scene again to reflect the changes
+  renderer.render(scene, camera);
+});
