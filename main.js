6434fb8994b0e68c82fb6b69cbf435e28734b6be
janitha-visna <visnajanitha@gmail.com>
2024-06-01 23:39:56 +0530

    cahnge the colour of the shpere according to mouse move

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
 const scene = new THREE.Scene();
 
 const geometry = new THREE.SphereGeometry(3.64, 64);
-const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+const material = new THREE.MeshStandardMaterial({ color: "#00ff83" ,roughness:0.5});
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -20,6 +20,7 @@ const sizes = {
 //light
 const light = new THREE.PointLight(0xffffff, 100, 100);
 light.position.set(0, 10, 10);
+
 scene.add(light);
 
 //camera
@@ -80,4 +81,30 @@ loop()
 const tl = gsap.timeline({defaults:{duration: 1}})
 tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
 tl.fromTo("nav", {y: "-100%"}, {y:"0%"})
-t1.fromTo(".title", {opacity:0}, {opacity:1})
+tl.fromTo(".title", {opacity:0}, {opacity:1})
+
+//mouse  animator colour
+let mouseDown = false
+let rgb = [];
+
+window.addEventListener("mousedown",() => (mouseDown=true))
+window.addEventListener("mouseup",() => (mouseDown = false))
+
+window.addEventListener('mousemove',(e) => {
+  if(mouseDown){
+    console.log(rgb)
+    rgb = [ Math.round((e.pageX/sizes.width)*255) ,
+      Math.round((e.pageY/sizes.height) * 255),
+      150,
+    ]
+
+    //animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color,{
+      r:newColor.r,
+      g:newColor.g,
+      b:newColor.b
+    })
+    
+  }
+})
